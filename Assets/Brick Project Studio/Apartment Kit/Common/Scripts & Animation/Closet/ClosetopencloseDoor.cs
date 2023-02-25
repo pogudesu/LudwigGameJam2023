fbb7d37e29e94ea4796e76a4e9d6862a44e3b5ed
Kevin7171993 <08093k06@gmail.com>
2023-02-25 06:47:51 -0800

    -Added custom dialogue "clicking" sound by leo to some character's dialogue -Added a prefab object with AudioSource Component -Edited AudioManager to hold the said prefab -Some doors that does not already have an AudioSource will now create an AudioSource for itself from the prefab -Ability to just drag and drop audio clips into OpenCloseDoor component to add sounds to the doors

--- Assets/Brick Project Studio/Apartment Kit/Common/Scripts & Animation/Closet/ClosetopencloseDoor.cs
+++ Assets/Brick Project Studio/Apartment Kit/Common/Scripts & Animation/Closet/ClosetopencloseDoor.cs
@@ -5,19 +5,42 @@ using UnityEngine;
 namespace SojaExiles
 
 {
-	public class ClosetopencloseDoor : MonoBehaviour
+	public class ClosetopencloseDoor : Interactables
 	{
 
 		public Animator Closetopenandclose;
 		public bool open;
 		public Transform PlayerControl;
+		public AudioSource audiosource;
+		public AudioClip sfx_open;
+		public AudioClip sfx_close;
 
-		void Start()
+		public override void Start()
 		{
 			open = false;
-			PlayerControl = PlayerController.Instance.transform;
+		}
+		public override void Update()
+		{
+			base.Update();
 		}
 
+		public override bool Interact()
+		{
+			if (distanceToPlayer < interactableDistance)
+			{
+				if (open == false)
+				{
+					StartCoroutine(opening());
+				}
+				else
+				{
+					StartCoroutine(closing());
+				}
+			}
+			return false;
+		}
+
+		/*
 		void OnMouseOver()
 		{
 			{
@@ -51,11 +74,16 @@ namespace SojaExiles
 			}
 
 		}
-
+		*/
 		IEnumerator opening()
 		{
 			print("you are opening the door");
 			Closetopenandclose.Play("ClosetOpening");
+			if (audiosource != null)
+			{
+				audiosource.clip = sfx_open;
+				audiosource.Play();
+			}
 			open = true;
 			yield return new WaitForSeconds(.5f);
 		}
@@ -64,6 +92,11 @@ namespace SojaExiles
 		{
 			print("you are closing the door");
 			Closetopenandclose.Play("ClosetClosing");
+			if (audiosource != null)
+			{
+				audiosource.clip = sfx_close;
+				audiosource.Play();
+			}
 			open = false;
 			yield return new WaitForSeconds(.5f);
 		}
